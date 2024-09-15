dec509080ed7c6477761930b78ac71a85da1abf9
esthermdev <esmd258@gmail.com>
2024-09-15 16:00:36 -0400

    Test preview update

--- app/index.js
+++ app/index.js
@@ -24,7 +24,6 @@ export default function App() {
       <View className='flex-2 px-[35] pt-5 pb-12'>
         <View className='mb-12'>
           <Text className='font-outfitregular text-center' style={{ fontSize: 20, }}>Welcome to</Text>
-          <Text className='font-outfitregular text-center' style={{ fontSize: 20, }}>Test Update</Text>
           <Text className='font-outfitbold text-center' style={{ fontSize: 38, marginVertical: 25 }}>Lobster Pot</Text>
           <Text className='font-outfitbold text-center' style={{ fontSize: 28 }}>2024</Text>
         </View>
